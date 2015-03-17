7506d5ea64afe26fa0d2078b43fd08ce129af08f
Vu Pham <anhvu.phamduong@gmail.com>
2015-03-16 23:28:19 -0700

    front page

--- client/template/account/account.js
+++ client/template/account/account.js
@@ -73,11 +73,10 @@ Template.signOut.events({
     }
 });
 Template.signIn.events({
-    'submit #signInForm': function (e, t) {
+    'click .signin': function (e, t) {
         e.preventDefault();
-        var signInForm = $(e.currentTarget),
-            email = trimInput(signInForm.find('.email').val().toLowerCase()),
-            password = signInForm.find('.password').val();
+        var email = trimInput($(' #signInForm .email ').val().toLowerCase()),
+            password = $(' #signInForm .password ').val();
         if (isNotEmpty(email) && isEmail(email) && isNotEmpty(password) && isValidPassword(password)) {
             Meteor.loginWithPassword(email, password, function (err) {
                 if (err) {
